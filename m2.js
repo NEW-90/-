90e62772c54a02a48dbcbd1990d9f69990e8d13e
NEW-90 <dlscam112@gmail.com>
2024-12-15 17:33:46 +0500

    one

---
+++ m2.js
@@ -0,0 +1,16 @@
+export function pl(a, b){
+    const res = Number(a) + Number(b);
+    console.log("result: ", res);
+}
+
+export function m(a, b){
+    return console.log("result: ", a - b);
+}
+
+export function k(a, b){
+    return console.log("result: ", a * b);
+}
+
+export function b(a, b){
+    return console.log("result: ", a / b);
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+import * as calc from './m2.js';
+
+calc.k(5, 5);
